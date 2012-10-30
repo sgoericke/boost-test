30a96acbbf820fe317e4a4a2121ef85f4da21dd9
Sven Goericke <sven@xpjets.com>
2012-10-30 23:51:53 +0100

    -- split parsing of log and result nodes

--- boost-test.el
+++ boost-test.el
@@ -22,30 +22,81 @@
 ;; Conventions used in this file: Name internal variables and functions
 ;; "boost-test--<descriptive-name>".
 
-;;(eval-when-compile (require 'cl))
+(eval-when-compile (require 'cl))
 
-(defun process--result(test result)
-  (let ((buf (get-buffer-create (concat "test results for: " (process-name test))))
-		(xml (concat "<Result>" result "</Result>")))
-	(message "%s" xml)
-	(set-buffer buf)
-	(erase-buffer)
-	(insert xml)
-	(let ((root (xml-parse-region (point-min) (point-max))))
-	  (message "%s" (car root))
-	  (switch-to-buffer buf))))
-;;	(let ((root (xml-parse-region (point-min) (point-max))))
-;;	  (erase-buffer)
-;;	  (message "%s" root))))
-;;	  (switch-to-buffer buf))))
-
-
+;;---------------------------------------------------------------------------------------------------------------------
 (defun boost-test-run ()
   "Runs the test specified"
   (interactive)
-  (let ((test-prog (read-file-name "Test program:")))
-	(let ((proc (start-process test-prog nil test-prog "--output_format=XML --log_level=test_suite")))
+  (let ((test-prog (read-file-name "Test program:"))
+		(buf (get-buffer-create (generate-new-buffer-name "*test-result*"))))
+	(let ((proc (start-process test-prog buf test-prog "--output_format=XML --log_level=test_suite")))
+	  (let ((sentinel (lambda (process signal)
+						(unwind-protect
+							(save-excursion
+							  (set-buffer (process-buffer process))
+							  (boost-test--process-result (buffer-substring 1 (point-max))))
+						  (kill-buffer (process-buffer process))))))
+		(set-process-sentinel proc sentinel)))))
 
-	  (set-process-filter proc 'process--result))))
+;;---------------------------------------------------------------------------------------------------------------------
+(defun boost-test--process-result (result)
+
+  (let* ((buf (get-buffer-create "boost::test results"))
+		 (root (with-temp-buffer
+				 (insert (concat "<Root>" result "</Root>"))
+				 (xml-parse-region (point-min) (point-max))))
+
+		 (result_root (car root))
+		 (test_log (car (xml-get-children result_root 'TestLog)))
+		 (test_result (car (xml-get-children result_root 'TestResult))))
+
+	(set-buffer buf)
+	(erase-buffer)
+
+	(boost-test--parse-log-node test_log buf)
+	(insert "\n")
+	(boost-test--parse-result-node test_result buf)
+	(switch-to-buffer buf)))
+
+;;---------------------------------------------------------------------------------------------------------------------
+(defun boost-test--parse-log-node (node buffer)
+  "Parse the TestLog part of the xml"
+  (cond ((null node) nil)
+		((listp node) (let ((elem (xml-node-name node))
+							(children (xml-node-children node)))
+
+						(cond ((string= "TestLog" elem) nil)
+
+							  ((string= "FatalError" elem) (let ((attr (xml-node-attributes node)))
+															 (insert (format "%s: " elem))
+															 (insert (format "%s "(car (last node))))
+															 (insert (format "[%s:%s:]\n"
+																			 (cdr (assq 'file attr))
+																			 (cdr (assq 'line attr))))))
+
+							  (t (cond ((stringp (car (last node))) (insert
+																	 (format "%s: %s\n" elem (car (last node)))))
+									   (t (let ((attr (xml-node-attributes node)))
+											(insert (format "%s: %s\n" elem (cdr (assq 'name attr)))))))))
+			 			(mapcar (lambda (x) (boost-test--parse-log-node x buffer)) children)))))
+						
+						
+;;---------------------------------------------------------------------------------------------------------------------
+(defun boost-test--parse-result-node (node buffer)
+  "Parse the TestResult part of the xml"
+
+  (when (listp node)
+	(let ((root (car (xml-get-children node 'TestSuite)))
+		  (attr (xml-node-attributes (car (xml-get-children node 'TestSuite))))
+		  (attr-count 0))
+
+	  (insert (format "%s result summary:\n" (car root)))
+
+	  (while (< attr-count (length (xml-node-attributes root)))
+		(insert (format "\t %s: %s\n"
+						(car (elt attr attr-count))
+						(cdr (elt attr attr-count))))
+		(setq attr-count (1+ attr-count))))))
 
 (provide 'boost-test)
